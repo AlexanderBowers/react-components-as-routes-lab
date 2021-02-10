75fd8867b7cb8022701c1220cee9687fd33e707d
AlexanderBowers <7319abowers@gmail.com>
2021-02-10 10:51:48 -0800

    finished lab

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,8 +4,21 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+    <h1>Directors Page</h1>
+    {directors.map(director=> {
+      return (
+        <div>
+      <h2>{`Name: ${director.name}`}</h2>
+      <p>Movies:</p>
+      <ul>
+        {director.movies.map(movie => {
+          return <li>{movie}</li>
+        })}
+      </ul>
+      </div>
+      )
+    })}
+  </div>
   );
 }
 
